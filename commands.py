ea77456d10542504344c01ad6b93c70a0bfedbfc
gusza110811 <auguz1108@gmail.com>
2025-10-04 03:41:27 +0700

    the server side is almost done

---
+++ commands.py
@@ -0,0 +1,37 @@
+import typing
+
+class Commands:
+    def __init__(self, socket:"socket.socket", server:"server.Server", clients:"list[server.Server]"):
+        self.socket = socket
+        self.server = server
+        self.clients = clients
+        self.mapping = {
+            "NAME" : self.NAME,
+            "MSG": self.MSG,
+            "LIST": self.LIST,
+            "QUIT": self.QUIT,
+        }
+
+    def NAME(self, args:list[str]):
+        self.server.username = args[1]
+
+    def MSG(self, args:list[str]):
+        if not self.server.username:
+            self.socket.send(b"ERR No username\n")
+            return
+        for client in self.clients:
+            client.recieve_message(args[1],self.server.username)
+
+    def LIST(self, args:list[str]):
+        self.socket.send(b"CTRL begin list\n")
+        for client in self.clients:
+            if not client.username: continue
+            self.socket.send((client.username+"\n").encode("ascii"))
+        self.socket.send(b"CTRL end list\n")
+
+    def QUIT(self,args:list[str]):
+        self.server.active = False
+        self.clients.remove(self.server)
+
+if typing.TYPE_CHECKING:
+    import socket, server
---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+import shlex
+import commands
+
+HOST = "192.168.1.118"
+PORT = 3000
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind((HOST,PORT))
+sock.listen(8)
+
+clients = []
+
+class Server(threading.Thread):
+    def __init__(self, sockt:tuple[socket.socket,tuple[str,int]]):
+        self.socket, self.address = sockt
+        global clients
+        self.commands = commands.Commands(self.socket,self, clients)
+        self.clients = clients # list is mutable so umm
+        self.clients.append(self)
+        super().__init__(target=self.run,daemon=True)
+
+        self.username = ""
+        self.channel = ""
+
+        self.active = False
+
+    def recieve_message(self, message, sender="*"):
+        self.socket.send(f"RECV {sender} : {message}\n".encode("ascii"))
+
+    def run(self):
+        self.active = True
+        sock = self.socket
+        commands = self.commands
+        sock.send(b"NOTE LF used for this connection\n")
+
+        while self.active:
+            command = shlex.split(sock.recv(512).decode())
+            if not command:
+                continue
+
+            try:
+                func = commands.mapping[command[0]]
+            except KeyError:
+                sock.send(b"ERR Invalid Command\n")
+            func(command)
+        
+        sock.close()
+
+
+
+if __name__ == "__main__":
+    while True:
+        connection = sock.accept()
+        server = Server(connection)
+        server.start()
